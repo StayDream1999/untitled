d972742f65ef346deb8f1ad8815c57fefd040841
StayDream1999 <StayDream1999@gmail.com>
2020-07-05 10:36:43 +0800

    Initial commit

---
+++ src/main/kotlin/STAY/SATY.kt
@@ -0,0 +1,14 @@
+package STAY
+
+fun main() {
+//    println("HELLO WORLD")
+//    Human().hello()
+    val h=Human()
+    h.Human()
+
+}
+class Human{
+    fun Human(){
+        print("hello kotlin")
+    }
+}
